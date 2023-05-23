c8fb86886330845cb95eef27d68bce4327c8141e
Ginajohana <124780437+Ginajohana@users.noreply.github.com>
2023-05-23 08:54:19 +0000

    primer commit agragando ruta nueva

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
+import { Link } from "react-router-dom";
 
 export const Home = () => (
 	<div className="text-center mt-5">
@@ -8,6 +9,7 @@ export const Home = () => (
 		<p>
 			<img src={rigoImage} />
 		</p>
+		<Link to={"register"} className="btn btn-warning">registrar usuario</Link>
 		<a href="#" className="btn btn-success">
 			If you see this green button, bootstrap is working
 		</a>
